826e2ee78d2b95bc8392d90ad13b853d5fd19ccf
yarinek <mareks726@gmail.com>
2021-02-03 20:51:55 +0100

    'homepage'

---
+++ src/pages/Start.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Start.css'
+
+function Start() {
+    return (
+        <div className='start'>
+            <p className='blink start__text'>START GAME</p>
+        </div>
+    )
+}
+
+export default Start
+
